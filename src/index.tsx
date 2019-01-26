72da8684b4aa5e57ac29fc236cf7cb723290a575
dzefa <danchong625@gmail.com>
2019-01-25 19:48:09 -0800

    [app] imports antd and css file to entry app

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import { render } from 'react-dom';
 
 import { Main } from './components/main/main';
 
+import 'antd/dist/antd.css';
+import './index.css';
+
 render(
   <Main />,
   document.getElementById('root')
