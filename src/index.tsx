793059de41f43eb70fb78a050fd8190d791f1045
dzefa <danchong625@gmail.com>
2019-01-25 23:23:16 -0800

    [feat] implements redux + persist + router onto app

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,25 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { history, configureStore } from './store';
 
 import { Main } from './components/main/main';
 
 import 'antd/dist/antd.css';
 import './index.css';
 
+const { store, persistor } = configureStore();
+
 render(
-  <Main />,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <ConnectedRouter history={history}>
+        <Main />
+      </ConnectedRouter>
+    </PersistGate>
+  </Provider>,
   document.getElementById('root')
 );
