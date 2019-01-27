c0c39ae22c0a0cf1621f1fb33ae6d12ec568d390
dzefa <danchong625@gmail.com>
2019-01-27 00:53:20 -0800

    [feat] initializes login component

---
+++ src/components/login/login.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+class Login extends React.Component<{}> {
+  constructor() {
+    super({});
+  }
+
+  render() {
+    return (
+      <div>
+        Login Page
+      </div>
+    )
+  }
+}
+
+export default Login;
